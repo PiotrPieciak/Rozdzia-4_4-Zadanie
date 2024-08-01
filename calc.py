d69438c8dc9ba1dde6ce2c36cede6dcb115c2d20
Piotr Pięciak <piotr.pieciak@gmail.com>
2024-08-01 11:57:46 +0200

    Add function in case of wrong data input

--- calc.py
+++ calc.py
@@ -6,13 +6,13 @@ def equation_multi_sum(operation,num_for_equ,result):
     if operation == "1":
         logging.info(f"Dodaję liczby: {num_for_equ} ")
         for i in num_for_equ:
-            result=result+i
+            result += i
         print(f"Wynik to {result}")
     else:
         result = 1 
         logging.info(f"Mnożę liczby: {num_for_equ} ")
         for i in num_for_equ:
-            result = result*i
+            result *= i
         print(f"Wynik to {result}")
 
 #Funkcja dla operacji odejmowania i dzielenia
@@ -23,6 +23,10 @@ def equation_subtract_devide(operation,first_number,second_number):
     else:
         logging.info("Dzielę %d przez %d" % (first_number,second_number))
         print(f"Wynik to {first_number / second_number}")
+
+#funkcja informująca o podaniu nieprawidłowej zmiennej
+def wrong_number():
+    print("Nie wprowadzono poprawnie liczby, spróbuj jeszcze raz. ")
 
 #Początek głównego programu
 if __name__ == "__main__":
@@ -35,7 +39,7 @@ if __name__ == "__main__":
         if  operation in ["1" ,"2" , "3" , "4"]:
             break
         else:
-            print("Niepoprawnie zdefiniowałeś działanie, to na prawdę proste - odszukaj na klawiaturze cyfry 1,2,3,4.")
+            wrong_number()
 
 # Dla Operacji Dodawania i mnożenia użyjemy osobnej funkcji gdyż wymagają zmiennej ilości składników.
     if operation in ["1","3"]:
@@ -48,7 +52,7 @@ if __name__ == "__main__":
                 else:    
                     break
             except:
-                print("Nie wprowadzono poprawnie liczby składników zdefiniowanego działania. Napisz dla ilu liczb chcesz wykonać zdefiniowane działanie.")
+                wrong_number()
         for i in range(count_number):
             while True:
                 num_for_equ.append(input("Podaj %d z %d liczb która będzie użyta do zdefiniowanego działania: " % (i+1,count_number)))
@@ -56,7 +60,7 @@ if __name__ == "__main__":
                     num_for_equ[i] = float(num_for_equ[i])
                     break
                 except:
-                    print("Nie wprowadzono poprawnie liczby, stać Cię na więcej...")
+                    wrong_number()
                     del num_for_equ[i]
         #Uruchamiamy fucnkcę wyłącznie dla przypadku mnożenia lub dodawania
         equation_multi_sum(operation,num_for_equ,result)
@@ -69,19 +73,21 @@ if __name__ == "__main__":
                 first_number = float(first_number)
                 break
             except:
-                print("Nie wprowadzono poprawnie liczby, stać Cię na więcej...")
+                wrong_number()
         while True:
             second_number = input("Podaj składnik 2. ")
             # Obsługa przypadku dzielenia przez zero
             if second_number == "0": 
-                print("Jesteś zdemoralizowany, chcesz dzielić przez zero!! Podaj inną liczbę. ")
+                wrong_number()
                 continue
             try:
                 second_number = float(second_number)
                 break
             except:
-                print("Nie wprowadzono poprawnie liczby, spróbuj jeszcze raz i nie poddawaj się...")
+                wrong_number()
         #Uruchamiamy fucnkcę wyłącznie dla przypadku dzielenia lub odejmowania
         equation_subtract_devide(operation,first_number,second_number)
 
-    
+
+
+       
